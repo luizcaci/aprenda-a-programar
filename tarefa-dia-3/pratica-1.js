6ec1bbc1ff22df6278e3b42c514f4ae9df093103
Luiz Caciraghi Silva <luizcaci@gmail.com>
2022-05-20 23:33:29 -0300

    Parabéns, você conseguiu!

--- tarefa-dia-3/pratica-1.js
+++ tarefa-dia-3/pratica-1.js
@@ -4,4 +4,6 @@ let profissão = 'vendedor'
 let idade = 27
 let viagemdossonhos = 'usa'
 
-console.log(nome, email, profissão, idade, viagemdossonhos)
+let mensagem = 'Olá, ' + nome + '! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ' + viagemdossonhos + '. Confirma pra gente alguns dados? A sua idade é ' + idade +', você é ' + profissão + ' e o seu email é ' + email + '? Ficamos no aguardo. Muito obrigado e boa viagem! :D'
+
+console.log (mensagem)
