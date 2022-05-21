e0b3a02ee72b7604ad4a9d04036328545b050a6d
Luiz Caciraghi Silva <luizcaci@gmail.com>
2022-05-20 23:21:59 -0300

    Parabéns, você conseguiu!

--- tarefa-dia-3/pratica-1.js
+++ tarefa-dia-3/pratica-1.js
@@ -1,5 +1,7 @@
 let nome = 'Luiz'
 let email = 'luizcaci@gmail.com'
 let profissão = 'vendedor'
-let idade = '27'
+let idade = 27
 let viagemdossonhos = 'usa'
+
+console.log(nome, email, profissão, idade, viagemdossonhos)
